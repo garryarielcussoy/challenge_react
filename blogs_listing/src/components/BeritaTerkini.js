33d4e18ebc0f0d4e6c9b2e56efcbfef482f9cb88
Garry Ariel <garry@alterra.id>
2020-01-06 17:17:56 +0700

    Berita Terkini done

---
+++ blogs_listing/src/components/BeritaTerkini.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import '../styles/bootstrap.min.css'
+import '../styles/main.css'
+
+let judulBeritaTerkini = [
+    "Gabung Alpha Tech Academy Sekarang!",
+    "Ada Apa Antara Kobar dan Hasan?",
+    "Mengenal Arafat Sang Master Python",
+    "Belajar React.Js Itu Seru. Kamu Setuju?",
+    "Sudahkah Kamu Sehat Mental?",
+];
+
+export class BeritaTerkini extends React.Component {
+    render(){
+        return (
+            <div className='berita-terkini'>
+                <div className='container-fluid'>
+                    <div className='row berita-terkini-first-row'>
+                        <div className='col-6 berita-terkini-headline'>
+                            <span>BERITA TERKINI</span>
+                        </div>
+                        <div className='col-6 lihat-semua'>
+                            <span>Lihat Semua</span>
+                        </div>
+                    </div>
+                    <div className='row list-berita-terkini'>
+                        {judulBeritaTerkini.map((element, i) => 
+                            <div className='col-12 elemen-list-berita-terkini'>
+                                <div><span className='tag-number'>#{i+1}</span></div>
+                                <div><span className='judul-berita-terkini'>{element}</span></div>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
--- blogs_listing/src/pages/MainPage.js
+++ blogs_listing/src/pages/MainPage.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import '../styles/bootstrap.min.css'
 import {Header} from '../components/Header'
+import {BeritaTerkini} from '../components/BeritaTerkini'
 
 export class MainPage extends React.Component {
     render() {
         return (
-            <Header />
+            <div>
+                <Header />
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-6'>
+                            <div className='container-fluid'>
+                                <div className='row'>
+                                    <div className='col-2'></div>
+                                    <div className='col-9'>
+                                        <BeritaTerkini />
+                                    </div>
+                                    <div className='col-1'></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
