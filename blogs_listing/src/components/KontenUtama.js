b8a60263ee1c03410f9dc68f874143f96daad378
Garry Ariel <garry@alterra.id>
2020-01-06 18:20:52 +0700

    Icons

--- blogs_listing/src/components/KontenUtama.js
+++ blogs_listing/src/components/KontenUtama.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import '../styles/bootstrap.min.css'
 import smsImage from '../img/sms.png'
+import heartIcon from '../img/heart_icon.png'
+import shareIcon from '../img/share_icon.png'
+import dislikeIcon from '../img/dislike_icon.png'
 
 export class KontenUtama extends React.Component {
     render(){
@@ -10,6 +13,13 @@ export class KontenUtama extends React.Component {
                 <h3 className='judul-konten-utama'>Nikahi Rakyat Jelata, Putri Ayako dari Jepang Lepaskan Gelar Kerajaan</h3>
                 <p className='isi-konten-utama'> Pernikahan Putri Ayako dan Kei Moriya dilangsungkan lewat upacara tradisional Jepang</p>
                 <p className='last-update'>Last updated 3 mins ago</p>
+                <div className='container-fluid love-share-dislike'>
+                    <div className='row'>
+                        <div className='col-4 love'><img className='a-pack-of-icons' src={heartIcon}/></div>
+                        <div className='col-4 share'><img className='a-pack-of-icons' src={shareIcon}/></div>
+                        <div className='col-4 dislike'><img className='a-pack-of-icons' src={dislikeIcon}/></div>
+                    </div>
+                </div>
             </div>
         )
     }
