ffa57aee612fbf2b2223004b58000d44c4e113aa
Garry Ariel <garry@alterra.id>
2020-01-06 15:58:47 +0700

    Work on Header

---
+++ blogs_listing/src/pages/MainPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/bootstrap.min.css'
+import {Header} from '../components/Header'
+
+export class MainPage extends React.Component {
+    render() {
+        return (
+            <Header />
+        )
+    }
+}
