e9d05c67e1cab29c75bb32a8a6e3c1d873006fda
Garry Ariel <garry@alterra.id>
2020-01-07 16:06:47 +0700

    Install react-router-dom

--- blogs_listing/src/components/SearchBar.js
+++ blogs_listing/src/components/SearchBar.js
@@ -4,13 +4,32 @@ import '../styles/main.css'
 import searchLogo from '../img/search.png'
 
 export class SearchBar extends React.Component {
+    state = {
+        keyWord: ""
+    }
     
-    
+    // componentDidMount = () => {
+    //     const self = this;
+    //     axios
+    //         .get(urlHeadline)
+    //         .then(function(response){
+    //             self.setState({judulBeritaTerkini: response.data.articles, isLoading: false})
+    //         })
+    //         .catch(function(response){
+    //             self.setState({isLoading: false})
+    //         })
+    // }
+
+    // Handle onChange
+    handleChange(event) {
+        this.setState({keyWord: event.target.value})
+    }
+
     render(){
         return (
             <div className="search-bar">
                 <form>
-                    <input type="text" placeholder="Search"></input>
+                    <input type="text" placeholder="Search" value={this.state.keyWord} onChange={this.handleChange}></input>
                     <button type="submit"><img src={searchLogo} className="search-logo" /></button>
                 </form>
             </div>
