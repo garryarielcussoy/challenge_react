520d54fe9812a97b84ec5fa9f39361bda6b796c5
Garry Ariel <garry@alterra.id>
2020-01-08 14:56:28 +0700

    Move out

---
+++ build/precache-manifest.5206142f9a9b46be3ca9696e2d033d3b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6179abfcbf902e8ec49ba47c47a067e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91390fb3f2c27e8508e7",
+    "url": "/static/css/main.2446de3a.chunk.css"
+  },
+  {
+    "revision": "370cddafb21b10e5a6cc",
+    "url": "/static/js/2.5c72fe51.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.5c72fe51.chunk.js.LICENSE"
+  },
+  {
+    "revision": "91390fb3f2c27e8508e7",
+    "url": "/static/js/main.b657b5a7.chunk.js"
+  },
+  {
+    "revision": "5cf180c74a9e09996804",
+    "url": "/static/js/runtime-main.4a0568a0.js"
+  },
+  {
+    "revision": "7c3876a770e78fd9c12d77ad731b31bb",
+    "url": "/static/media/dislike_icon.7c3876a7.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "760ad844919aaf691d0307c99f23e93f",
+    "url": "/static/media/share_icon.760ad844.png"
+  }
+]);
